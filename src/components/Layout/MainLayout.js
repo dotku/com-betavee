e2a97c9d858230c5591e83929ae6f4011b475bb4
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-06-13 07:59:20 +0000

    add margin to the header

--- src/components/Layout/MainLayout.js
+++ src/components/Layout/MainLayout.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
     return <div className="container">
-        <h1>贝塔维科技有限公司 betavee</h1>
+        <h1 className="my-3">贝塔维科技有限公司 betavee</h1>
         <Outlet />
     </div>
 }
